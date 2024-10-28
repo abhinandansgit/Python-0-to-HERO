3c8bacf7fb2edfaab218995745083d2adbeaa164
Abhinandan Parhi <127218863+abhinandansgit@users.noreply.github.com>
2024-10-29 01:06:35 +0530

    Create problem_5.py

---
+++ chapter_1/practice_set/problem_5.py
@@ -0,0 +1,22 @@
+# Label the program written in problem 4 with comments. 
+
+import os
+#os module provides the system file access to the interpreter
+def print_directory_contents(directory_path):
+
+#   Prints the contents of a given directory.
+
+#   Args:
+#     directory_path: The path to the directory.
+# use os module to view the directory contents
+
+  for item in os.listdir(directory_path):
+    item_path = os.path.join(directory_path, item)
+    if os.path.isdir(item_path):
+      print(f"{item_path}/")
+      print_directory_contents(item_path)
+    else:
+      print(item_path)
+#take input of path from user
+directory_path = input("Enter the directory path: ")
+print_directory_contents(directory_path)
