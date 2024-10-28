6f93e1b3ddf6e223bf3678b825845a04d4e504ea
Abhinandan Parhi <127218863+abhinandansgit@users.noreply.github.com>
2024-10-29 01:02:17 +0530

    Create problem_2.py

---
+++ chapter_1/practice_set/problem_2.py
@@ -0,0 +1,10 @@
+# Use REPL and print the table of 5 using it. 
+'''C:\Users\hp>python
+Python 3.12.4 | packaged by Anaconda, Inc. | (main, Jun 18 2024, 15:03:56) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 5*1,5*2
+(5, 10)
+>>> 5*1,5*2,5*3,5*4,5*5,5*6,5*7,5*8,5*9,5*10
+(5, 10, 15, 20, 25, 30, 35, 40, 45, 50)
+>>>
+'''
